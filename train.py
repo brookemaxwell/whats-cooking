f27bdd9b772bed5f12a7d17209ffdc20a77cf63c
Bryant Chandler <brychanrobot@gmail.com>
2015-10-31 15:39:56 -0600

    added pipeline stuff for naive bayes and SGD(Whatever that is)

--- train.py
+++ train.py
@@ -1,7 +1,10 @@
 import json
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.decomposition import SparsePCA
-from sklearn.cross_validation import cross_val_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
+from sklearn import metrics
 import re
 from sklearn import tree
 from numpy import *
@@ -25,11 +28,11 @@ def parse_recipe_file(filename, feature_vectorizer=None, label_set=None):
         ingredient_string = re.sub('\d+', '', ingredient_string)  # delete numbers
         raw_features.append(ingredient_string)
 
-    if feature_vectorizer is None:
-        feature_vectorizer = CountVectorizer()
-        instances = feature_vectorizer.fit_transform(raw_features)
-    else:
-        instances = feature_vectorizer.transform(raw_features)
+    #if feature_vectorizer is None:
+    #    feature_vectorizer = CountVectorizer()
+    #    instances = feature_vectorizer.fit_transform(raw_features)
+    #else:
+    #    instances = feature_vectorizer.transform(raw_features)
 
     #print(feature_vectorizer.get_feature_names())
     #print(instances.toarray())
@@ -41,31 +44,94 @@ def parse_recipe_file(filename, feature_vectorizer=None, label_set=None):
     for label in raw_labels:
         labels.append(label_set.index(label))
 
-    return instances, labels, feature_vectorizer, label_set
+    return raw_features, labels, label_set
 
-def export_tree(decision_tree, name):
-    dot_data = StringIO()
-    tree.export_graphviz(decision_tree, out_file=dot_data)
-    graph = pydot.graph_from_dot_data(dot_data.getvalue())
-    graph.write_pdf(name + '.pdf')
+
+def split_test_and_training(features, labels, percent_training=0.7):
+    num_training_instances = int(percent_training * len(labels))
+
+    training_features = features[:num_training_instances]
+    test_features = features[num_training_instances:]
+
+    training_labels = labels[:num_training_instances]
+    test_labels = labels[num_training_instances:]
+
+    return training_features, training_labels, test_features, test_labels
+
+
+def sgd():
+    text_clf = Pipeline([('vect', CountVectorizer()),
+                         ('tfidf', TfidfTransformer()),
+                         ('sgd', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42))
+                         ])
+
+    return text_clf
+
+
+def naive_bayes():
+    text_clf = Pipeline([('vect', CountVectorizer()),
+                         ('tfidf', TfidfTransformer()),
+                         ('clf', MultinomialNB())
+                         ])
+
+    return text_clf
+
 
 def main():
     training_filename = 'Data/train.json'
 
-    training_instances, training_labels, feature_vectorizer, label_set = parse_recipe_file(training_filename)
-    print("file parsed")
+    features, labels, label_set = parse_recipe_file(training_filename)
+    print("file_parsed")
+
+    training_features, training_labels, test_features, test_labels = split_test_and_training(features, labels)
+
 
     """
-    pca = SparsePCA(n_components=100)
-    pca_training_instances = pca.fit_transform(training_instances.toarray())
-    print("pca complete")
+    countVectorizer = CountVectorizer()
+
+    c_training_features = countVectorizer.fit_transform(training_features)
+
+
+    tfidf = TfidfTransformer()
+    tfidf_training_features = tfidf.fit_transform(c_training_features)
+
+    clf = MultinomialNB().fit(tfidf_training_features, training_labels)
+
+
+    c_test_features = countVectorizer.transform(test_features)
+
+    tfidf_test_features = tfidf.transform(c_test_features)
+
+    correct_count = 0
+    for index, instance in enumerate(tfidf_test_features):
+        if clf.predict(instance) == test_labels[index]:
+            correct_count += 1
+
+
+    accuracy = correct_count / double(len(test_labels))
+
+    print(accuracy)
     """
 
-    dt = tree.DecisionTreeClassifier(random_state=0)
-    score = cross_val_score(dt, training_instances, training_labels, cv=10)
+    text_clf = sgd()
+
+    text_clf.fit(training_features, training_labels)
+
+    predicted = text_clf.predict(test_features)
+    accuracy = mean(predicted == test_labels)
+
+    print(accuracy)
+
+    print(metrics.classification_report(test_labels, predicted, target_names=label_set))
+
+
+
+
+    #dt = tree.DecisionTreeClassifier(random_state=0)
+    #score = cross_val_score(dt, training_instances, training_labels, cv=10)
     # dt = dt.fit(training_instances.toarray(), training_labels)
 
-    print(score)
+    #print(score)
 
 
 if __name__ == '__main__':
