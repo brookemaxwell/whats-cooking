e94e86194906b05e744ce8ceb6aa8861c4f328c0
Bryant Chandler <brychanrobot@gmail.com>
2015-11-07 14:08:53 -0700

    added kaggle output

--- train.py
+++ train.py
@@ -9,8 +9,8 @@ import re
 from sklearn import tree
 from numpy import *
 from sklearn.externals.six import StringIO
-import pydot
 import random
+import csv
 
 
 def parse_recipe_file(filename, feature_vectorizer=None, label_set=None):
@@ -20,31 +20,29 @@ def parse_recipe_file(filename, feature_vectorizer=None, label_set=None):
 
     raw_labels = []
     raw_features = []
+    ids = []
 
     for recipe in recipes:
-        raw_labels.append(recipe['cuisine'])
+        if 'cuisine' in recipe.keys():
+            raw_labels.append(recipe['cuisine'])
+        else:
+            ids.append(recipe['id'])
         ingredient_string = ' '.join(recipe['ingredients'])  # make comma separated string
         ingredient_string = re.sub('\(\w+\)', '', ingredient_string)  # delete quantities in parentheses
         ingredient_string = re.sub('\d+', '', ingredient_string)  # delete numbers
         raw_features.append(ingredient_string)
 
-    #if feature_vectorizer is None:
-    #    feature_vectorizer = CountVectorizer()
-    #    instances = feature_vectorizer.fit_transform(raw_features)
-    #else:
-    #    instances = feature_vectorizer.transform(raw_features)
+    if len(raw_labels) != 0:
+        if label_set is None:
+            label_set = list(set(raw_labels))
 
-    #print(feature_vectorizer.get_feature_names())
-    #print(instances.toarray())
+        labels = []
+        for label in raw_labels:
+            labels.append(label_set.index(label))
 
-    if label_set is None:
-        label_set = list(set(raw_labels))
-
-    labels = []
-    for label in raw_labels:
-        labels.append(label_set.index(label))
-
-    return raw_features, labels, label_set
+        return raw_features, labels, label_set
+    else:
+        return raw_features, ids
 
 
 def split_test_and_training(features, labels, percent_training=0.7):
@@ -77,47 +75,16 @@ def naive_bayes():
     return text_clf
 
 
-def main():
-    training_filename = 'Data/train.json'
-
-    features, labels, label_set = parse_recipe_file(training_filename)
+def try_local(filename, algorithm):
+    features, labels, label_set = parse_recipe_file(filename)
+    label_set = asarray(label_set)
     print("file_parsed")
 
     training_features, training_labels, test_features, test_labels = split_test_and_training(features, labels)
 
+    algorithm.fit(training_features, training_labels)
 
-    """
-    countVectorizer = CountVectorizer()
-
-    c_training_features = countVectorizer.fit_transform(training_features)
-
-
-    tfidf = TfidfTransformer()
-    tfidf_training_features = tfidf.fit_transform(c_training_features)
-
-    clf = MultinomialNB().fit(tfidf_training_features, training_labels)
-
-
-    c_test_features = countVectorizer.transform(test_features)
-
-    tfidf_test_features = tfidf.transform(c_test_features)
-
-    correct_count = 0
-    for index, instance in enumerate(tfidf_test_features):
-        if clf.predict(instance) == test_labels[index]:
-            correct_count += 1
-
-
-    accuracy = correct_count / double(len(test_labels))
-
-    print(accuracy)
-    """
-
-    text_clf = sgd()
-
-    text_clf.fit(training_features, training_labels)
-
-    predicted = text_clf.predict(test_features)
+    predicted = algorithm.predict(test_features)
     accuracy = mean(predicted == test_labels)
 
     print(accuracy)
@@ -125,13 +92,30 @@ def main():
     print(metrics.classification_report(test_labels, predicted, target_names=label_set))
 
 
+def try_kaggle(training_filename, test_filename, algorithm):
+
+    training_features, training_labels, label_set = parse_recipe_file(training_filename)
+    label_set = asarray(label_set)
+
+    algorithm.fit(training_features, training_labels)
+
+    test_features, test_ids = parse_recipe_file(test_filename)
+    test_predicted = algorithm.predict(test_features)
+
+    with open('kaggle_out.csv', 'w') as file:
+        writer = csv.writer(file, lineterminator='\n')
+        writer.writerow(["id", "cuisine"])
+        data = list(zip(test_ids, label_set[test_predicted]))
+        writer.writerows(data)
 
 
-    #dt = tree.DecisionTreeClassifier(random_state=0)
-    #score = cross_val_score(dt, training_instances, training_labels, cv=10)
-    # dt = dt.fit(training_instances.toarray(), training_labels)
+def main():
+    training_filename = 'Data/train.json'
+    kaggle_test_filename = 'Data/test.json'
 
-    #print(score)
+    text_clf = sgd()
+    try_local(training_filename, text_clf)
+    #try_kaggle(training_filename, kaggle_test_filename, text_clf)
 
 
 if __name__ == '__main__':
